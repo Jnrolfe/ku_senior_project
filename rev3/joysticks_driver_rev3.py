54c7284040fcc8c25db0008466b5c63ea357a74a
James Rolfe <jamesnrolfe@gmail.com>
2018-03-06 16:11:55 -0600

    Updated directory structure along with updates to all drivers and new standard driver file

--- rev3/joysticks_driver_rev3.py
+++
@@ -1,9 +0,0 @@
-'''
-@filename: joysticks_driver_rev3.py
-@author: james_rolfe
-@updated: 20180205
-@about: This contains the joysticks driver code for the rev3 board
-'''
-
-# use the following link for build:
-# https://www.raspberrypi-spy.co.uk/2014/04/using-a-joystick-on-the-raspberry-pi-using-an-mcp3008/
